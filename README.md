a273752d2050bfd6f44b51fcb6499ae2a35aec46
IKostric <ivica_kostric@yahoo.com>
2023-05-30 10:23:02 +0200

    Update to the latest version from recsys21

--- README.md
+++ README.md
@@ -9,6 +9,36 @@ Another main contribution is the development of four models for the question gen
 Using common metrics from machine translation for automatic evaluation, we show that our approaches are effective in generating elicitation questions, even with limited training data.  
 We further employ human evaluation for comparing the generated questions using both pointwise and pairwise evaluation designs. We find that the human evaluation results are consistent with the automatic ones, allowing us to draw conclusions about the quality of the generated questions with certainty.  Finally, we provide a detailed analysis of cases where the models show their limitations.
 
+## Data
+
+We are not allowed to re-distribute the [Amazon review collection](https://nijianmo.github.io/amazon/index.html). Instead, we distribute our dataset with the review IDs (item ID and user ID) and sentence offsets. Additionally, we provide a script that derives the data collection we used from the original Amazon collection.
+
+### Dataset Structure
+
+The dataset contains 1100 reviews with matching implicit questions over 11 categories of products. It is split evenly into train (80%) and test (20%) files.
+The test file additionally contains 15 questions in the `Birdfeeder` category that is not found in train dataset.
+
+Since the sentences mentioning item usage were extracted heuristically, not all reviews in the dataset have valid questions associated with them. Those are marked as `n/a`.
+
+The files have the following entries:
+
+  - `id`: unique identifier
+  - `category`: A category the item belongs to.
+  - `question1`, `question2`, `question3`: Different variations of the usage question based on the review sentence obtained by crowdsourcing.
+  - `paraphrase1`, `paraphrase2`: Question rewrites obtained by crowdsourcing where the input were `question1`, `question2`, and `question3` and not the review sentence.
+  - `reviewerID`: Reviewer ID.
+  - `asin`: product ID.
+  - `start_index`: Start index of the extracted sentence.
+  - `end_index`: End index of the extracted sentence.
+
+Top 3 rows from the training dataset:
+
+| Id         | category     | question1 |question2 |question3 |paraphrase1 |paraphrase2 |reviewerID|asin|start_index|end_index|
+|--------------|-----------|------------|------------|------------|------------|------------|------------|------------|------------|------------|
+| 307...3NL         | Tent     | Are you interested in a tent that is perfect for backpacking and biking? |do you want a tent who is perfect for backpacking and biking? |Do you want a tent that's perfect for backpacking and biking? |Can you use a tent that is great for both biking and hiking? |Do you want a comfortable cycling tent? |A2P8B5PMOIE7W|B00A8E2F88|0|34|
+| 30E...6YK         | Walk-behind lawnmower     | Would you like a walk-behind lawnmower able to handle big yards? |Do you want a walk-behind lawnmower that can mow a big yard? |Are you looking for walk-behind lawnmower to mow a big yard? |Need a lawnmower that can mow a big yard? |How does a walk behind lawnmower to mow a big yard sound? |AEEI3GYQ5R0O5|B00Q2MGO32|80|139|
+| 32T...84N         | Bike     | Are you looking for a bike that is great for commuting? |Would you like a bike that is good for commuting? |Do you want a bike that is great for commuting? |Are you interested in purchasing a bike that makes it easy for commuting? |Do you want a bike that can be used for commuting? |A2RLVLI4RIXPW8|B004Q3N0GI|0|84|
+
 ## Repository Structure
 
 This repository is structured as follows:
@@ -21,9 +51,9 @@ This repository is structured as follows:
 
 ## Obtaining the dataset
 
-To obtain the full dataset use command:
+To obtain the full dataset, use command:
 
-```
+```bash
 python -m code.make_dataset --path <path_to_amazon_collection_folder>
 ```
 
--- code/make_dataset.py
+++ code/make_dataset.py
@@ -9,12 +9,13 @@ import os
 from typing import Dict, Iterator
 
 import pandas as pd
+from tqdm import tqdm
 
-REVIEW_FILENAMES = [
-    "Patio_Lawn_and_Garden.json.gz",
-    "Home_and_Kitchen.json.gz",
-    "Sports_and_Outdoors.json.gz",
-]
+REVIEW_FILENAMES = {
+    "Patio_Lawn_and_Garden.json.gz": 5236058,
+    "Home_and_Kitchen.json.gz": 21928568,
+    "Sports_and_Outdoors.json.gz": 12980837,
+}
 
 DATASET_PATH = "dataset/"
 
@@ -57,36 +58,56 @@ def load_dataset(filename: str) -> pd.DataFrame:
     return pd.read_csv(os.path.join(DATASET_PATH, filename), index_col=False)
 
 
-def add_reviews(dataset: pd.DataFrame, folder_path: str) -> pd.DataFrame:
+def save_dataset(dataset: pd.DataFrame, filename: str) -> None:
+    """Saves Pandas DataFrame to file.
+
+    Args:
+        dataset: DataFrame to save to file
+        filename: Filename to use when saving DataFrame to file.
+    """
+    dataset.to_csv(
+        os.path.join(DATASET_PATH, f"{filename}_full.csv"),
+        index=False,
+    )
+
+
+def add_reviews(datasets: pd.DataFrame, folder_path: str) -> pd.DataFrame:
     """Iterates over the 2018 Amazon collection line by line and adds the review
     text to the dataset if user ID and item ID match.
 
     Args:
-        df: DataFrame without full review texts.
+        dataset: DataFrame without full review texts.
         folder_path: Path to the 2018 Amazon collection folder containing files
             specified in REVIEW_FILENAMES.
 
     Returns:
         Pandas DataFrame including full review texts and extracted sentences.
     """
-    counter = 0
-    for filename in REVIEW_FILENAMES:
-        print(filename)
-        for i, item in enumerate(parse_jsonl(os.path.join(folder_path, filename))):
-            matches = (dataset["asin"].values == item["asin"]) & (
-                dataset["reviewerID"].values == item["reviewerID"]
-            )
-            if any(matches):
-                match = df.loc[matches].iloc[0]
+    for filename, total in REVIEW_FILENAMES.items():
+        for i, item in enumerate(
+            tqdm(parse_jsonl(os.path.join(folder_path, filename)), total=total)
+        ):
+            for dataset in datasets:
+                matches = (dataset["asin"].values == item["asin"]) & (
+                    dataset["reviewerID"].values == item["reviewerID"]
+                )
+
+                if not any(matches):
+                    continue
+
                 dataset.loc[matches, "reviewText"] = item["reviewText"]
-                dataset.loc[matches, "sentenceText"] = item["reviewText"][
-                    match["start"] : match["end"]
+                dataset.loc[matches, "sentenceText"] = [
+                    item["reviewText"][start:end]
+                    for start, end in zip(
+                        dataset[matches].start_index, dataset[matches].end_index
+                    )
                 ]
-                counter += 1
-            if (i + 1) % 100000 == 0:
-                print(f"Parsed {i} items.")
-    print(f"Done. \nSuccesfully updated {counter}/{len(dataset)} items.")
-    return df
+        if i + 1 != total:
+            print(
+                f"Number of lines in {filename} ({i+1}) does not match the "
+                f"expected number of lines ({total})."
+            )
+    return datasets
 
 
 def main(args: argparse.Namespace) -> None:
@@ -96,16 +117,21 @@ def main(args: argparse.Namespace) -> None:
     Args:
         args: Command line arguments. Should contain path to the Amazon dataset.
     """
-    for filename in ["train", "test"]:
-        df = load_dataset(f"{filename}.csv")
-        df = add_reviews(df, folder_path=args.path)
-        df.to_csv(f"{filename}_full.csv", index=False)
+    train = load_dataset("train.csv")
+    test = load_dataset("test.csv")
+    train, test = add_reviews([train, test], folder_path=args.path)
+    save_dataset(train, "train")
+    save_dataset(test, "test")
 
 
 def parse_arguments() -> argparse.Namespace:
     """Parses command line arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--path", "-p", help="Path to the 2018 Amazon dataset folder.")
+    parser.add_argument(
+        "--path",
+        "-p",
+        help="Path to the 2018 Amazon dataset folder.",
+    )
     return parser.parse_args()
 
 
